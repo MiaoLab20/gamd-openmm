06afad93fcd70c2af6cf8469c553d6c923e28b2a
Matthew Copeland <matthew@ku.edu>
2020-05-06 10:52:06 -0500

    * Addition of some commented out debugging statements. * Ability to change the output directory that will be created on execution.

--- clean-gamd-runner.py
+++ clean-gamd-runner.py
@@ -1,18 +1,20 @@
-7#!/usr/bin/python3
-
 from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
 from sys import exit
 import os
+import sys
 import traceback
 from gamd.langevin.total_boost_integrators import GamdTotalPotentialBoostLangevinLowerBoundIntegrator
 from gamd import utils as utils
+import pprint
+
 
 def create_output_directories(directories):
     for dir in directories:
         os.makedirs(dir, 0o755)
+
 
 def getGlobalVariableNames(integrator):
     for index in range(0, integrator.getNumGlobalVariables()):
@@ -21,9 +23,13 @@ def getGlobalVariableNames(integrator):
 
 def main():
 
-    output_directory = "output"
+    if len(sys.argv) == 1:
+        output_directory = "output"
+    else:
+        output_directory = sys.argv[1]
+
     coordinates_file = './data/md-4ns.rst7'
-    prmtop_file =  './data/dip.top'
+    prmtop_file = './data/dip.top'
 
     create_output_directories([output_directory, output_directory + "/states/", output_directory + "/positions/",
                                output_directory + "/pdb/", output_directory + "/checkpoints"])
@@ -35,6 +41,12 @@ def main():
     prmtop = AmberPrmtopFile(prmtop_file)
     inpcrd = AmberInpcrdFile(coordinates_file)
     system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds)
+
+    # group = 1
+    # for force in system.getForces():
+    #     print(force.__class__.__name__)
+    #     force.setForceGroup(group)
+    #     group += 1
 
 #    integrator = GamdTotalPotentialBoostLangevinLowerBoundIntegrator(2.0 * femtoseconds, 2000, 10000, 2000, 10000,
 #                                                                     30000, 500)
@@ -53,8 +65,11 @@ def main():
                                                   potentialEnergy=True, totalEnergy=True, volume=True))
     gamdLog = []
     for step in range(1, integrator.get_total_simulation_steps() + 1):
+    # for step in range(1, 3):
         try:
+            # print(integrator.get_current_state())
             simulation.step(1)
+            # print(integrator.get_current_state())
 #            if step > 9400 and step < 10100:
 #                print("Step ", str(step), " -  Energy Start:  ", integrator.get_boost_potential(), ", End: ",
 #                      integrator.get_current_potential_energy())
@@ -66,9 +81,12 @@ def main():
             print(integrator.get_current_state())
             sys.exit(2)
 
+        # debug_information = integrator.get_debugging_information()
+
+
         # getGlobalVariableNames(integrator)
         if step % integrator.ntave == 0:
-        #if step % 1 == 0:
+        # if step % 1 == 0:
             simulation.saveState(output_directory + "/states/" + str(step) + ".xml")
             simulation.saveCheckpoint(output_directory + "/checkpoints/" + str(step) + ".bin")
             gamdLog.append({'total_nstep': step,
@@ -77,8 +95,10 @@ def main():
                             'Boost-Energy-Potential': integrator.get_boost_potential()})
             positions_filename = output_directory + '/positions/coordinates-' + str(step) + '.csv'
             integrator.create_positions_file(positions_filename)
-    utils.create_gamd_log(gamdLog, output_directory + "/gamd.log")
 
+    utils.create_gamd_log(gamdLog, output_directory + "/gamd.log")
+    # pp = pprint.PrettyPrinter(indent=2)
+    # pp.pprint(debug_information)
 
 if __name__ == "__main__":
     main()
