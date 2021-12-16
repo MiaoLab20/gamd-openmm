7a9959587aa7a445ee04ed302f7cca9e29cec30e
Lane Votapka <lvotapka100@gmail.com>
2021-12-16 11:31:59 -0700

    Added dependencies in setup.py

--- setup.py
+++ setup.py
@@ -51,12 +51,13 @@ setup(
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    install_requires=["numpy", "parmed", "pytest", "matplotlib"], 
+    # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
     #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
+     python_requires=">=3.7",          # Python version restrictions
 
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     # zip_safe=False,
